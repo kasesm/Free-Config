8ed03b5a7ce9757d7de89c6f6df32722c16063c9
kasesm <task132141@gmail.com>
2026-02-27 20:07:39 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -84,7 +84,7 @@ normal_channels = [
     'FreeConfigV2ray_1', 'v2rayfresh', 'v2ray_youtube_group/10', 'v2rayfreedaily', 'outlineOpenKey',
     'PrivateVPNs', 'VlessConfig', 'vmessiraan', 'vmesskhodam', 'vmessh', 'config_ss','config_v2ray_daily',
     'prrofile_purple', 'v2_mod_shop', 'anty_filter', 'YamYamProxy', 'ettehad_vpn', 'DarkTeam_VPN', 'iran_v2ray1',
-    'samiotech', 'Hope_Net',
+    'samiotech', 'Hope_Net', 'ProxyFa10',
 ]
 
 print(f"{'Channel Name':<25} | {'Count':<10}")
